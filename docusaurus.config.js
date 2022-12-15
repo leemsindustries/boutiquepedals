0e538eb7a80de441a1774c64be4e7d5b8ed16741
SEIBERT Lee <XJY@dsone.3ds.com>
2022-12-15 16:05:21 -0500

    A few cosmetic fixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,6 +81,11 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      colorMode: {
+          defaultMode: 'light',
+          disableSwitch: true,
+          respectPrefersColorScheme: false,
+      },
     }),
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,13 +12,13 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <h1 className="hero__title myHeroTitle">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Start browsing the directory
+            Start browsing the list
           </Link>
         </div>
       </div>
